7e7e4d3a836afa248cbb6b2be5d52c1857c6c060
David <davidesv69@gmail.com>
2023-01-13 11:06:07 -0600

    Ejercicio 1 LiveCode

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // Escribe tu código aquí.
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+
+function bigWords(word, array) {
+    return array.filter(xd => xd.length > word.length);
+}
+
+function printArray(array) {
+    const list = document.getElementById('myList');
+    list.innerHTML = array.map(xd => `<li>${xd}</li>`).join('');
+}
+
+const submitButton = document.getElementById('submitButton');
+submitButton.addEventListener('click', () => {
+    const inputWord = document.getElementById('inputWord').value;
+    const bigWordsArray = bigWords(inputWord, myArray);
+    printArray(bigWordsArray);
+});
